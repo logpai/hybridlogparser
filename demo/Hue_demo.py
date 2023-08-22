e748c827d353404bd61afc26078956d28f3cb690
Xl <xl@XldeMacBook-Pro.local>
2023-08-22 10:36:29 +0800

    --Update=Redundant statements

--- demo/Hue_demo.py
+++ demo/Hue_demo.py
@@ -10,6 +10,3 @@ odir = 'results/Hue_result/'
 parser = Hue.Parser(idir, odir, config='parsers/Hue/config_HiBench.yml',feedback=feedback)
 parser.parse('HiBench_2k.log')
 parser.print()
-
-gt = '../../datasets/HiBench/HiBench_2k.log_structured.log'
-pd = odir + 'meta.log'
